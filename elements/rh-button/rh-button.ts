848e59eca3afac76e36a8bc924f07475520130a1
HadassahYelenik <hlyelenik@gmail.com>
2025-09-15 16:17:03 +0300

    Revert whitespace changes in rh-button.ts

--- elements/rh-button/rh-button.ts
+++ elements/rh-button/rh-button.ts
@@ -13,7 +13,6 @@ import { InternalsController } from '@patternfly/pfe-core/controllers/internals-
 
 import styles from './rh-button.css';
 
-
 /**
  * A button is clickable text or an icon that triggers an action on the page or in the background.
  * Depending on the action, content, and hierarchy, a button can be used on its own or grouped with
